494c80ec19a4e4aa1f82f41e17012d2312be74f9
Saikat Taleb <matsaikat@gmail.com>
2023-01-13 13:44:24 +0100

    Added CORS to Spring Security Filter to process CORS first

--- src/main/java/com/tjv/project/bookclub/BookclubApplication.java
+++ src/main/java/com/tjv/project/bookclub/BookclubApplication.java
@@ -2,12 +2,28 @@ package com.tjv.project.bookclub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class BookclubApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BookclubApplication.class, args);
+    }
+    @Bean
+    SecurityFilterChain securityConfig(HttpSecurity security) throws Exception {
+        security.cors()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**")
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .oauth2ResourceServer().jwt();
+        return security.build();
     }
 
 }
