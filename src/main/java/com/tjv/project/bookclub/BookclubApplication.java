9951678429e3ac753f102960f11456dfd76cdedc
Saikat Taleb <matsaikat@gmail.com>
2023-06-12 00:48:24 +0200

    modularized CORS configuration for easier setup

--- src/main/java/com/tjv/project/bookclub/BookclubApplication.java
+++ src/main/java/com/tjv/project/bookclub/BookclubApplication.java
@@ -1,19 +1,10 @@
 package com.tjv.project.bookclub;
 
-import com.tjv.project.bookclub.config.CustomCorsConfigurationSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 
 @SpringBootApplication
 public class BookclubApplication {
